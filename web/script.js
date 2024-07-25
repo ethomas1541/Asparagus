ed53ac1e6260a129addd7543e4b5230e36f26f97
kayleet <kayleekthomas@gmail.com>
2024-07-25 13:44:20 -0700

    Show description now editable. Added comments to add_show()

--- web/script.js
+++ web/script.js
@@ -1,15 +1,20 @@
 function add_show(){
+
+    // Create list element to hold show card
     let show_card = document.createElement("li");
+    
+    // Set list element class and id
     show_card.className = "show";
     show_card.id = "show card";
 
+    // Fill in show card
     show_card.innerHTML = innerHTML = `
         <h2>Show Name</h2>
         <div class="showbox">
             <div class="img-frame">
                 <img src="img/sample.png">
             </div>
-            <div class="show-description">
+            <div class="show-description" contenteditable="true">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ab hic, sunt non quaerat eveniet voluptate asperiores doloribus modi veritatis temporibus inventore officiis facere sed aut facilis aspernatur. Est, at. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur at est, architecto repellendus repellat maiores odit. Harum necessitatibus quia sunt ex esse laboriosam, veniam deserunt. Adipisci perspiciatis suscipit qui vitae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam minima ea numquam placeat. Necessitatibus, eos pariatur beatae explicabo suscipit architecto commodi dolorem odit, error temporibus, quidem ab corrupti consequuntur repellat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores facere mollitia fuga illo repellendus voluptate ratione alias cum aliquid quo reiciendis, consequatur debitis odio excepturi, est asperiores laborum non architecto?</p>
             </div>
             <div class="progbar">
@@ -28,6 +33,8 @@ function add_show(){
             </div>
         </div>
     `;
+
+    // Append element to end of list
     document.getElementById("show_list").appendChild(show_card);
 
 }
