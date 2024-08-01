79e252717dd645d42bc2153763f18ce679c1e8f2
CalebSutherland <sutherlandcaleb12@gmail.com>
2024-07-31 22:34:23 -0700

    TVDB API automatically fills the description and thumbnail when adding a show

--- app/static/script.js
+++ app/static/script.js
@@ -1,4 +1,74 @@
-function add_show() {
+async function getAuthToken(apiKey) {
+    const url = 'https://api4.thetvdb.com/v4/login';
+    const payload = { apikey: apiKey };
+    const headers = { 'Content-Type': 'application/json' };
+
+    try {
+        const response = await axios.post(url, payload, { headers });
+        if (response.data && response.data.data && response.data.data.token) {
+            return response.data.data.token;
+        } else {
+            throw new Error("Authentication failed: 'token' not found in the response");
+        }
+    } catch (error) {
+        console.error("Authentication error:", error.response ? error.response.data : error.message);
+        throw error;
+    }
+}
+
+async function searchSeries(token, seriesName) {
+    const url = `https://api4.thetvdb.com/v4/search?query=${encodeURIComponent(seriesName)}`;
+    const headers = {
+        'Authorization': `Bearer ${token}`,
+        'Accept': 'application/json'
+    };
+
+    try {
+        const response = await axios.get(url, { headers });
+        console.log(response.data); // Debugging line to check the data structure
+        return response.data;
+    } catch (error) {
+        console.error("Search series error:", error.response ? error.response.data : error.message);
+        throw error;
+    }
+}
+
+async function add_show() {
+    const API_KEY = 'fefac935-f11a-4342-87fc-59e8ee37995e';
+    let token;
+
+    try {
+        token = await getAuthToken(API_KEY);
+    } catch (error) {
+        console.error('Failed to get auth token:', error);
+        return;
+    }
+
+    const seriesName = prompt("Enter the name of the show:");
+    if (!seriesName) return;
+
+    let searchResults;
+    try {
+        searchResults = await searchSeries(token, seriesName);
+    } catch (error) {
+        console.error('Failed to search for series:', error);
+        return;
+    }
+
+    let firstResult;
+    let seriesDescription;
+    let seriesImage;
+
+    if (searchResults.data && searchResults.data.length > 0) {
+        firstResult = searchResults.data[0];
+        seriesDescription = firstResult.overview || "Description not available.";
+        seriesImage = firstResult.image_url ? firstResult.image_url : "static/img/sample.png";
+    } else {
+        firstResult = { name: seriesName };
+        seriesDescription = "No information available.";
+        seriesImage = "static/img/sample.png";
+    }
+
     // Create list element to hold show card
     let show_card = document.createElement("li");
 
@@ -8,42 +78,42 @@ function add_show() {
 
     // Fill in show card with html elements
     show_card.innerHTML = `
-        <h2 contenteditable="true" spellcheck="false">Show Name</h2>
-            <div class="showbox">
-                <div class="img-frame">
-                    <img src="static/img/sample.png">
-                </div>
-                <div class="show-description" contenteditable="true">
-                    <p spellcheck="false">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ab hic, sunt non quaerat eveniet voluptate asperiores doloribus modi veritatis temporibus inventore officiis facere sed aut facilis aspernatur. Est, at. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur at est, architecto repellendus repellat maiores odit. Harum necessitatibus quia sunt ex esse laboriosam, veniam deserunt. Adipisci perspiciatis suscipit qui vitae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam minima ea numquam placeat. Necessitatibus, eos pariatur beatae explicabo suscipit architecto commodi dolorem odit, error temporibus, quidem ab corrupti consequuntur repellat? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores facere mollitia fuga illo repellendus voluptate ratione alias cum aliquid quo reiciendis, consequatur debitis odio excepturi, est asperiores laborum non architecto?</p>
-                </div>
-                <div class="remove-show-container">
-                    <button class="remove-show" onclick="remove_show(this)">Remove Show</button>
-                </div>
-                <div class="progbar">
-                    <input class="current-ep" type="number" dir="rtl" min="0" maxlength="4" placeholder="####">
-                    <p id="divider">/</p>
-                    <p id="total-ep">22</p>
-                    <div class="meter-bg"></div>
-                    <div class="meter"></div>
-                </div>
-                <div class="ratings">
-                    <div class="star-bg">
-                        <div class="star-darkgray"></div>
-                        <div class="star-yellow"></div>
-                        <img src="static/img/starframe.png">
-                        <div class = "star-buttons">
-                            <button id = "b1star"></button>
-                            <button id = "b2star"></button>
-                            <button id = "b3star"></button>
-                            <button id = "b4star"></button>
-                            <button id = "b5star"></button>
-                        </div>
+        <h2 contenteditable="true" spellcheck="false">${firstResult.name}</h2>
+        <div class="showbox">
+            <div class="img-frame">
+                <img src="${seriesImage}">
+            </div>
+            <div class="show-description" contenteditable="true">
+                <p spellcheck="false">${seriesDescription}</p>
+            </div>
+            <div class="remove-show-container">
+                <button class="remove-show" onclick="remove_show(this)">Remove Show</button>
+            </div>
+            <div class="progbar">
+                <input class="current-ep" type="number" dir="rtl" min="0" maxlength="4" placeholder="####">
+                <p id="divider">/</p>
+                <p id="total-ep">22</p>
+                <div class="meter-bg"></div>
+                <div class="meter"></div>
+            </div>
+            <div class="ratings">
+                <div class="star-bg">
+                    <div class="star-darkgray"></div>
+                    <div class="star-yellow"></div>
+                    <img src="static/img/starframe.png">
+                    <div class="star-buttons">
+                        <button id="b1star"></button>
+                        <button id="b2star"></button>
+                        <button id="b3star"></button>
+                        <button id="b4star"></button>
+                        <button id="b5star"></button>
                     </div>
-                    <div class="number-bg">
-                        <input class="rating" type="number" placeholder="--" min="0.0" max="5.0" step="0.5">
-                    </div>
+                </div>
+                <div class="number-bg">
+                    <input class="rating" type="number" placeholder="--" min="0.0" max="5.0" step="0.5">
                 </div>
             </div>
+        </div>
     `;
 
     // Add show card to top of list
@@ -51,7 +121,7 @@ function add_show() {
     list.insertBefore(show_card, list.children[0]);
 }
 
-function remove_show(elemRef){
+function remove_show(elemRef) {
     /*  elemRef is the element that called remove_show
         IMPORTANT! if remove-show button is moved will need to update!
     */
@@ -88,7 +158,6 @@ function levenshtein(a, b) {
     return matrix[b.length][a.length];
 }
 
-
 function searchShows() {
     const searchTerm = document.getElementById('searchBar').value.toLowerCase();
     const shows = Array.from(document.querySelectorAll('#show_list .show'));
@@ -118,10 +187,10 @@ function clearSearch() {
     });
 }
 
-function acc_login(){
+function acc_login() {
     alert("Login Implementation Coming Soon...");
 }
 
-function create_acc(){
+function create_acc() {
     alert("Account Creation Implementation Coming Soon...");
 }
