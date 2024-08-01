f82a8ce4bebec165d5b7d6b34cdf189e63accf26
Elijah <ethomas1541@gmail.com>
2024-08-01 00:10:56 -0700

    Initial database implementation / dockerization

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient("mongodb://mongo", 27017)
+db = client.mydb
+collection = db.test732
+
+collection.insert_one({
+    "foo": "bar",
+    "bar": "foo"
+})
+
+print(list(collection.find({})))
 
 @app.route("/")
 def default():
